5923d2c42c8eb59b326e112c024f05f51c8a1b11
Vishal Soni <84185041+vishalsnitk@users.noreply.github.com>
2024-07-05 13:17:32 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
-from flask import Flask  #here flask is our module and from there we import the Flask class.
+from flask import Flask, render_template
+#flask: here flask is our module and from there we import the Flask class.
+# render_template: use to run html code
+
 
 app = Flask(__name__)
-#once flask application created we need to give it name   .... here name is refering to main     #
+#Flask(__name__): once flask application created we need to give it name   .... here name is refering to main     #
 # print(__name__)               #it will print the main
 
 #Any site will be having some url so when certain url is requested what it should return that we need to say to the flask
+JOBS= [
+  {
+    "id":1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+  },
+  {
+    "id":2,
+    "title": "SDE",
+    "location": "Pune",
+    "salary": "Rs. 14,00,000"
+    
+  },
+  {
+    "id":3,
+    "title": "Influencer",
+    "location": "Mumbai",
+    "salary": "Rs. 14,00,000,0000"
 
+  }
+]
 
 @app.route("/")  #way to perfrom the routing "/"
 def hello_world():
-  return "<p>Hello, World!</p>"
-
+  return render_template('home.html',jobs= JOBS)
+  
+#Data: data is send from list to html
 
 #This is just way to run the flask application
 if __name__ == "__main__":
